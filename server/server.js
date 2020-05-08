ccdef69713d9525ccfcf9b41c07e995f323b6181
maduiep <emmanuelmaduip@gmail.com>
2020-05-08 22:21:50 +0100

    second commit

--- server/server.js
+++ server/server.js
@@ -2,7 +2,23 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('<h1>Hello World!</h1>'))
+app.get('/', (req, res) => res.statusCode(404).send({
+ error: 'Page not found',
+ name: 'Todo App v1.0'
+}))
+
+app.get('/users', (req, res) => {
+ res.send([{
+ name: 'Madu',
+ age: 27
+ }, {
+ name: 'Jamsine',
+ age: '24'
+  }, {
+   name: 'Courage',
+   age: 27
+}])
+})
 app.listen(port, () => console.log(` listening to sever port ${port}!`))
 
 module.exports.app = app
--- server/server.test.js
+++ server/server.test.js
@@ -1,9 +1,29 @@
 const request = require('supertest')
+const expect = require('expect')
 
 const app = require('./server').app
 
-it('should return hello world response', (done) => {
+it('should return Hello World response', (done) => {
  request(app)
   .get('/')
-  .expect('Hello Wolrd')
+  .expect(404)
+  .expect((res) => {
+   expect(res.body).toInclude({
+    error: 'Page not found'
+   })
+  })
+  .end(done)
+})
+
+it('should retun my user object', (done) => {
+ request(app)
+  .get('/users')
+  .expect(200)
+  .expect((res) => {
+   expect(res.body).toInclude({
+    name: 'Emmanuel',
+    age: 27
+   })
+  .end(done)
+  })
 })
